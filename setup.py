a08e7c499e7ae232baf1dfa4c7f0eba53e7fb87f
hIghwAvE314 <hw19159@bristol.ac.uk>
2025-01-04 09:49:15 +0000

    update package structure

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='RCWA',
     version='0.1',
-    packages=find_packages(),
+    packages=[
+        "rcwa",
+        "rcwa.geometries",
+    ],
 )
